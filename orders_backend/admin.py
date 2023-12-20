a16c4b0637e099b75bbec167ac5f0ca5f9cb0461
Arturo-MT <ingatorrez@gmail.com>
2023-12-20 16:28:07 -0600

    Add Store and Payment models

--- orders_backend/admin.py
+++ orders_backend/admin.py
@@ -2,15 +2,11 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import User, Order, Product, OrderItem
+from .models import User, Order, Product, OrderItem, Payment, Store
 
 admin.site.register(User)
 admin.site.register(Order)
 admin.site.register(Product)
-
-@admin.register(OrderItem)
-class OrderItemAdmin(admin.ModelAdmin):
-    list_display = ('order', 'product', 'quantity')
-    list_filter = ('order', 'product')
-    search_fields = ('order__customer__name', 'product__name')
-    list_per_page = 20
+admin.site.register(OrderItem)
+admin.site.register(Payment)
+admin.site.register(Store)
--- orders_backend/models.py
+++ orders_backend/models.py
@@ -5,10 +5,31 @@ class User(models.Model):
     email = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=10)
     password = models.CharField(max_length=50)
+    is_staff = models.BooleanField(default=False)
+    store = models.ForeignKey('Store', on_delete=models.CASCADE, null=True, blank=True)
+
+class Store(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=10)
+    email = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    preview = models.ImageField(upload_to='products', null=True, blank=True)
+    available = models.BooleanField(default=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
-    description = models.TextField(blank=True, null=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True, blank=True)
     
     STATUS_CHOICES = [
         ('R', 'Received'),
@@ -30,19 +51,22 @@ class Order(models.Model):
     def __str__(self):
         return f'Pedido {self.id} - {self.customer.username}'
 
-class Product(models.Model):
-    name = models.CharField(max_length=50)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    preview = models.ImageField(upload_to='products', null=True, blank=True)
-    available = models.BooleanField(default=True)
-
-    def __str__(self):
-        return self.name
-
 class OrderItem(models.Model):
     order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+    description = models.TextField(null=True, blank=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'Item {self.quantity} - {self.product.name}'
+    
+class Payment(models.Model):
+    order = models.ForeignKey(Order, related_name='payments', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    payment_date = models.DateTimeField(auto_now_add=True)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return f'Pagamento {self.id} - Pedido {self.order.id} - {self.amount}'
+    
