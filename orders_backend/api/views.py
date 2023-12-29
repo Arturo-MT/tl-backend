dcafc51f0a1a5617587467d81205e0a095a233ef
Arturo-MT <ingatorrez@gmail.com>
2023-12-29 16:16:32 -0600

    Add permissions and validation for store and product creation, update, and deletion

--- orders_backend/api/views.py
+++ orders_backend/api/views.py
@@ -2,9 +2,10 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.exceptions import PermissionDenied
 from orders_backend.models import Product, User, Store, Order, OrderItem
 from .serializers import ProductSerializer, UserSerializer, StoreSerializer, OrderSerializer, OrderItemSerializer
-from .permissions import IsSelfOrStaffOrSuperuser
+from .permissions import IsSelfOrStaffOrSuperuser, IsStoreOwnerOrReadOnly, IsProductOwnerOrReadOnly
 
 class UserCreateView(CreateAPIView):
     serializer_class = UserSerializer
@@ -18,24 +19,70 @@ class UserDetailView(RetrieveUpdateDestroyAPIView):
 class StoreViewSet(ModelViewSet):
     queryset = Store.objects.all()
     serializer_class = StoreSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsStoreOwnerOrReadOnly]
 
     def get_queryset(self):
-        user = self.request.user
-        if user.is_superuser:
-            return Store.objects.all()
-        return Store.objects.filter(owner=user)
+        return Store.objects.all()
+    
+    def perform_create(self, serializer):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied("You must be logged in to create a store.")
+        
+        if not self.request.user.is_seller:
+            raise PermissionDenied("You must be a seller to create a store.")
+        if serializer.validated_data.get('owner') != self.request.user:
+            raise PermissionDenied("You can only create a store for yourself.")
+        serializer.save(owner=self.request.user)
+
+    def perform_update(self, serializer):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied("You must be logged in to update a store.")
+        
+        if serializer.validated_data.get('owner') != self.request.user:
+            raise PermissionDenied("You can only update a store that you own.")
+        serializer.save(owner=self.request.user)
+    
+    def perform_destroy(self, instance):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied("You must be logged in to delete a store.")
+        
+        if instance.owner != self.request.user:
+            raise PermissionDenied("You can only delete a store that you own.")
+        instance.delete()
 
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsProductOwnerOrReadOnly]
 
     def get_queryset(self):
-        user = self.request.user
-        if user.is_superuser:
-            return Product.objects.all()
-        return Product.objects.filter(store__owner=user)
+        return Product.objects.all()
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied("You must be logged in to add products to a store.")
+        
+        store = serializer.validated_data.get('store')
+        if store.owner != self.request.user:
+            raise PermissionDenied("You must be the owner of this store to add products to it.")
+        serializer.save(store=store)
+    
+    def perform_update(self, serializer):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied("You must be logged in to update products in a store.")
+        
+        store = serializer.validated_data.get('store')
+        if store.owner != self.request.user:
+            raise PermissionDenied("You must be the owner of this store to update its products.")
+        serializer.save(store=store)
+    
+    def perform_destroy(self, instance):
+        if not self.request.user.is_authenticated:
+            raise PermissionDenied("You must be logged in to delete products from a store.")
+        
+        if instance.store.owner != self.request.user:
+            raise PermissionDenied("You must be the owner of this store to delete its products.")
+        instance.delete()
     
 class OrderAndOrderItemPagination(PageNumberPagination):
     page_size = 20
